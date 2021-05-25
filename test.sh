a01cf206e5bcba1f22776cf4338ee518650145fd
0x2b3bfa0 <0x2b3bfa0+git@googlemail.com>
2021-05-25 18:16:03 +0200

    A

--- test.sh
+++ test.sh
@@ -4,8 +4,14 @@ function checkpoint() {
   touch "$FLAG" && while test -f "$FLAG"; do true; done
 }
 
+test -f test.data || echo 0 > test.data
+echo "Initial state $(cat test.data)"
+
 for iteration in {1..10}; do
-    ln -s /dev/random test.data
+    echo $(($(cat test.data)+1)) > test.data
+    echo "New state $(cat test.data)"
     checkpoint
     sleep 1
 done
+
+echo "Finished!"
