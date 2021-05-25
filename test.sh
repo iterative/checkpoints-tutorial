9a20c587cbe0cf0c2873d7c71083491b1beccaed
0x2b3bfa0 <0x2b3bfa0+git@googlemail.com>
2021-05-25 15:23:53 +0000

    dvc: commit experiment faf9a28b19eaeaa03bddc7201a80ff7b092ddc1e1638af1d53745dad990869c8

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
+function checkpoint() {
+  [[ -z "$DVC_ROOT" ]] && return
+  FLAG="$DVC_ROOT/.dvc/tmp/DVC_CHECKPOINT"
+  touch "$FLAG" && while test -f "$FLAG"; do true; done
+}
+
+
 for iteration in {0..100}; do
-    date +%s > test.data
-    sleep 5
+    date +%s | tee test.data
+    checkpoint
+    sleep 1
 done
